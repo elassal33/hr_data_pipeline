ddf35bb18f5b53db5ca61186974439bc79177e1d
elassal33 <adham.elassal22@gmail.com>
2026-02-03 06:21:04 +0200

    Refactor hr_employee_clean procedure for incremental load
    Updated the procedure to insert data instead of creating a new table. Modified the logic for determining gender and termination status, and adjusted the data selection criteria for incremental loading.

--- snowflake/silver.sql
+++ snowflake/silver.sql
@@ -1,3 +1,20 @@
+CREATE TABLE IF NOT EXISTS silver.hr_employee_clean (
+    EMPID INT,
+    EMPLOYEE_NAME STRING,
+    GENDER STRING,
+    POSITION STRING,
+    DEPARTMENT STRING,
+    STATE STRING,
+    SALARY NUMBER,
+    IS_TERMINATED BOOLEAN,
+    EMPLOYMENTSTATUS STRING,
+    DATEOFHIRE DATE,
+    DATEOFTERMINATION DATE,
+    LOAD_DATE DATE
+);
+
+
+
 CREATE OR REPLACE PROCEDURE silver.sp_build_hr_employee_clean()
 RETURNS STRING
 LANGUAGE SQL
@@ -5,16 +22,16 @@ AS
 $$
 BEGIN
 
-    CREATE OR REPLACE TABLE silver.hr_employee_clean AS
+    INSERT INTO silver.hr_employee_clean
     SELECT
         EMPID,
-        TRIM(EMPLOYEE_NAME) AS EMPLOYEE_NAME,
+        TRIM(EMPLOYEE_NAME),
 
         CASE
             WHEN SEX = 'M' THEN 'MALE'
             WHEN SEX = 'F' THEN 'FEMALE'
             ELSE 'UNKNOWN'
-        END AS GENDER,
+        END,
 
         POSITION,
         DEPARTMENT,
@@ -24,23 +41,24 @@ BEGIN
         CASE
             WHEN TERMED = 1 THEN TRUE
             ELSE FALSE
-        END AS IS_TERMINATED,
+        END,
 
         EMPLOYMENTSTATUS,
         DATEOFHIRE,
         DATEOFTERMINATION,
         LOAD_DATE
-    FROM (
-        SELECT *,
-               ROW_NUMBER() OVER (
-                   PARTITION BY EMPID
-                   ORDER BY LOAD_DATE DESC
-               ) AS rn
-        FROM bronze.hr_employee_raw
-    )
-    WHERE rn = 1;
+    FROM bronze.hr_employee_raw
+    WHERE LOAD_DATE >
+        COALESCE(
+            (SELECT MAX(LOAD_DATE) FROM silver.hr_employee_clean),
+            '1900-01-01'
+        );
 
-    RETURN 'Silver HR clean completed';
+    RETURN 'Silver HR incremental clean completed';
 
 END;
 $$;
+
+
+
+
